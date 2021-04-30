fffeae001f1bb3ab8ae765b8bae90d26d1655059
JulianFernandezCisilino <julian91fernandez@gmail.com>
2021-04-30 11:34:52 +0200

    Bootstrap theme

---
+++ client/src/app/_modules/shared.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ToastrModule } from 'ngx-toastr';
+
+
+//Este modulo "shared" se usa para manter el app.module mas limpio
+//se agrega la referencia a este modulo en app.module
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BsDropdownModule.forRoot(),
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right'
+    })
+  ],
+  exports: [
+    BsDropdownModule,
+    ToastrModule
+  ]
+})
+export class SharedModule { }
